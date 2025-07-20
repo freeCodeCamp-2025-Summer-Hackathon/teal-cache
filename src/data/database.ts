b115457bfc8dc843079ea0ba6a93341e499cfbaa
cosmonewt <jeyang90@gmail.com>
2025-07-20 17:38:43 +0100

    fix: updated database.ts

--- src/data/database.ts
+++ src/data/database.ts
@@ -4,23 +4,6 @@ import { supabase } from "../lib/supabaseClient";
 // Select all journal prompts
 export const selectAll = async () => {
 
-    const selectAllQueryTest = supabase
-                                    .from('journal_entry')
-                                    .select(`
-                                        *, 
-                                        journal_content(text, title), 
-                                        journal_prompt(
-                                            prompt, 
-                                            journal_prompt_category(name)
-                                        ), 
-                                        emoji(
-                                            name, 
-                                            shortcode,
-                                            unicode,
-                                            utf8
-                                        )`
-                                    );
-
     const selectAllQuery = supabase
                                     .from('journal_entry')
                                     .select(`
@@ -37,8 +20,7 @@ export const selectAll = async () => {
     if (error) throw error
 
     console.log(data);
-    
-    // return data;
+    return data;
 };
 
 // Select single journal prompt
@@ -62,14 +44,12 @@ export const selectOne = async (userId: string) => {
     const { data, error } = await selectOneQuery;
     if (error) throw error
 
-    console.log(data);
-
-    // return data;
-
+    return data;
 };
 
+// WIP
 // Insert one journal entry
-export const insertOne = async () => {
+export const insertOne = async (inputObject) => {
     // TODO: define input object structure
     // journal prompt prompt/id, emoji name/id, content title and text, user email
 
@@ -78,81 +58,116 @@ export const insertOne = async () => {
     const selectJournalPromptQuery = supabase
                                         .from('journal_prompt')
                                         .select()
-                                        .eq('prompt_id', '')
-                                        .eq('prompt', '')
+                                        .eq('id', inputObject.journal_prompt_id)
+                                        .limit(1)
                                         .single();
                                         
     const { data: journalPromptData, error:journalPromptError } = await selectJournalPromptQuery
     if (journalPromptError) throw journalPromptError
-
-    console.log(journalPromptData);    
 
     // emoji
     // get emoji id
     const selectEmojiQuery = supabase
                                 .from('emoji')
                                 .select()
-                                .eq('name', '')
+                                .eq('id', inputObject.emoji_id)
+                                .limit(1)
                                 .single();
 
     const { data: emojiData, error: emojiError } = await selectEmojiQuery
     if (emojiError) throw emojiError
     
-    console.log(emojiData);    
-    
     // journal_content
     // insert journal content title and text
     const insertJournalContentQuery = supabase
                                         .from('journal_content')
-                                        .insert({title: '', text: ''})
+                                        .insert({title: inputObject.journal_content.title, text: inputObject.journal_content.text})
                                         .select()
+                                        .limit(1)
                                         .single();
 
     const { data: journalContentData, error: journalContentError } = await insertJournalContentQuery
     if (journalContentError) throw journalContentError
 
-    console.log(journalContentData);
-    
     // journal_entry
     // get user email
     // insert journal context id, -journal prompt id, and emoji id
     const insertJournalEntryQuery = supabase
                                         .from('journal_entry')
-                                        .insert({user_id: '', journal_content_id: '', journal_prompt_id: '', emoji_id: ''})
+                                        .insert({user_id: inputObject.user_email, journal_content_id: journalContentData.id, journal_prompt_id: journalPromptData.id, emoji_id: emojiData.id})
                                         .select()
+                                        .limit(1)
                                         .single();
 
     const { data: journalEntryData, error: journalEntryError } = await insertJournalEntryQuery
     if (journalEntryError) throw journalEntryError
 
-    console.log(journalEntryData);
-    
+    console.log(`Successfully created journal entry:\n`, journalEntryData);
 };
 
+
 // Update journal prompt
-export const updateOne = async () => {
+export const updateOne = async (inputObject) => {
     // TODO: define input object structure
+    // If input cannot containt ids, need to consider getting ids
     // journal prompt prompt/id, emoji name/id, content title and text, user email
 
-    // get ids?
-    // if input can contain relevant ids, it would be easier to update the tables
 
+    
+    
+    // update journal prompt
+    const selectJournalPromptQuery = supabase
+                                        .from('journal_prompt')
+                                        .select()
+                                        .eq('id', inputObject.journal_prompt_id)
+                                        .limit(1)
+                                        .single();
+                                        
+    const { data: journalPromptData, error:journalPromptError } = await selectJournalPromptQuery
+    if (journalPromptError) throw journalPromptError
+    
+    // update emoji
+    const selectEmojiQuery = supabase
+                                .from('emoji')
+                                .select()
+                                .eq('id', inputObject.emoji_id)
+                                .limit(1)
+                                .single();
+
+    const { data: emojiData, error: emojiError } = await selectEmojiQuery
+    if (emojiError) throw emojiError
 
     // update journal content
+    const insertJournalContentQuery = supabase
+                                        .from('journal_content')
+                                        .update({title: inputObject.journal_content.title, text: inputObject.journal_content.text})
+                                        .eq('id', inputObject.journal_content.id)
+                                        .select()
+                                        .limit(1)
+                                        .single();
 
-
-    // update journal prompt
-
-
-    // update emoji
-
+    const { data: journalContentData, error: journalContentError } = await insertJournalContentQuery
+    if (journalContentError) throw journalContentError
 
     // update journal entry
+    const insertJournalEntryQuery = supabase
+                                        .from('journal_entry')
+                                        .update({user_id: inputObject.user_email, journal_content_id: journalContentData.id, journal_prompt_id: journalPromptData.id, emoji_id: emojiData.id})
+                                        .eq('id', inputObject.id)
+                                        .select()
+                                        .limit(1)
+                                        .single();
 
+    const { data: journalEntryData, error: journalEntryError } = await insertJournalEntryQuery
+    if (journalEntryError) throw journalEntryError
+
+    console.log(`Successfully updated journal entry:\n`, journalEntryData);
 
 };
 
 // Delete journal entry
+// Deletes row from journal entry table and retains data in,
+// journal content, journal prompt, 
 export const deleteOne = async (inputId: string) => {
     const deleteOneQuery = supabase
                                 .from('journal_entry')
@@ -162,10 +177,10 @@ export const deleteOne = async (inputId: string) => {
     const { data, error } = await deleteOneQuery
     if (error) throw error
 
-    console.log(data);
-    // return data;
+    return data;
 };
 
+// Select all emojis
 export const selectAllEmojis = async () => {
     const allEmojisQuery = supabase
                                 .from('emoji')
